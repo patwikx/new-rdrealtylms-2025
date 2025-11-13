5dd7af5680ac38df2f72110b32c5da007a9c5156
Patrick Miranda <plmiranda@rdrealty.com.ph>
2025-11-13 08:26:51 +0800

    added mrs coordinator fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "sonner";
 import { SessionProvider } from "next-auth/react";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "RD Realty Group - PMS",
